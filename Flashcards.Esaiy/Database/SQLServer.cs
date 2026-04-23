43e9f0762903adadbf866063ecff77e66dbf96f2
Esaiy <93sultanbaney@gmail.com>
2026-04-23 12:27:28 +0700

    database migration

---
+++ Flashcards.Esaiy/Database/SQLServer.cs
@@ -0,0 +1,67 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Flashcards.Esaiy.Database;
+
+public class SQLServer(string connString)
+{
+    private readonly string connectionString = connString;
+
+    public void MigrateUp()
+    {
+        try
+        {
+            using var connection = new SqlConnection(connectionString);
+            var stackQuery = @"
+                    IF OBJECT_ID('dbo.stack', 'U') IS NULL
+                    BEGIN
+                      CREATE TABLE stack
+                      (
+                        Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+                        Name NVARCHAR(100) UNIQUE NOT NULL
+                      );
+                    END";
+            connection.Execute(stackQuery);
+
+            var flashcardQuery = @"
+                    IF OBJECT_ID('dbo.flashcard', 'U') IS NULL
+                    BEGIN
+                      CREATE TABLE flashcard
+                      (
+                        Id INT IDENTITY(1,1) PRIMARY KEY,
+                        Front NVARCHAR(255) NOT NULL,
+                        Back NVARCHAR(255) NOT NULL,
+                        StackId INT NOT NULL,
+                        CONSTRAINT FK_flashcard_stack FOREIGN KEY (StackId)
+                        REFERENCES stack (Id)
+                        ON DELETE CASCADE
+                      );
+                    END";
+            connection.Execute(flashcardQuery);
+        }
+        catch
+        {
+            Console.WriteLine("Failed creating initial databases");
+            throw;
+        }
+    }
+
+    public void MigrateDown()
+    {
+        try
+        {
+            using var connection = new SqlConnection(connectionString);
+            var dropQuery = @"
+                    DROP TABLE IF EXISTS flashcard;
+                    DROP TABLE IF EXISTS stack;";
+            connection.Execute(dropQuery);
+        }
+        catch
+        {
+            Console.WriteLine("Failed deleting databases");
+            throw;
+        }
+    }
+}
+
+
---
+++ Flashcards.Esaiy/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Flashcards.Esaiy.Database;
+Console.WriteLine("Hello, World!");
+
+var connectionString = "Server=localhost;Database=flashcard;User Id=sa;Password=P@ssword123;TrustServerCertificate=True";
+
+var SQLServerObject = new SQLServer(connectionString);
+SQLServerObject.MigrateUp();
